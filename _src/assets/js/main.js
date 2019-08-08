724dffe78c0985831a16832d03662e92c45e2f19
saramarin23 <saramarin.econ@gmail.com>
2019-08-08 19:18:44 +0200

    add function that shows if I click a show is favorite or not

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,6 +5,7 @@
 console.log(">> Ready :)");
 
 let savedSeries = [];
+let favoriteShows = [];
 
 const input = document.querySelector(".js-input");
 
@@ -26,10 +27,6 @@ function searchSeries() {
     });
 }
 
-//   for (let item of divSerie) {
-//     item.addEventListener("click", addToFavs);
-//   }
-
 function formatData(data) {
   let results = [];
   for (let i = 0; i < data.length; i++) {
@@ -43,19 +40,19 @@ function formatData(data) {
 
 function saveData(data) {
   savedSeries = data;
-  console.log(savedSeries);
+  //console.log(savedSeries);
 }
 
 function showData() {
   const series = document.querySelector(".js-series_section");
   let seriesToAdd = "";
   for (let item = 0; item < savedSeries.length; item++) {
-    seriesToAdd += `<div class="serie_element"><p class="show_title">${
+    seriesToAdd += `<div class="serie_element" data-index="${item}"><p class="show_title">${
       savedSeries[item].name
     }</p><img class="show_image" src="${savedSeries[item].image}" /></div>`;
   }
   series.innerHTML = seriesToAdd;
-  console.log(series);
+  //console.log(series);
 }
 
 function listenShow() {
@@ -65,8 +62,33 @@ function listenShow() {
   }
 }
 
-function favClick() {
+function favClick(ev) {
   console.log("Hey, I just clicked a serie div!");
+  const seriesIndex = showClicked(ev);
+  if (isThisShowFav(seriesIndex)) {
+    //Borramos de favoritos
+  } else {
+    //Añadimos a favoritos
+  }
+  showData();
+  listenShow();
+}
+
+function showClicked(ev) {
+  const currentTarget = ev.currentTarget;
+  const showClickedIndex = parseInt(currentTarget.dataset.index);
+  console.log(currentTarget); //Devuélveme el índice de lo que he clickado
+  return showClickedIndex;
+}
+
+function isThisShowFav(item) {
+  const numberIndex = favoriteShows.indexOf(item);
+  if (numberIndex >= 0) {
+    //Si existe
+    console.log(`Check if numberIndex ${item} is fav >>`, true);
+  } else {
+    console.log(`Check if numberIndex ${item} is not fav >>`, false);
+  }
 }
 
 const btn = document.querySelector(".js-btn");
