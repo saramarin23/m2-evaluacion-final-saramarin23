64ad75a449259b1ed2c0ab6a432c69778aa854e5
saramarin23 <saramarin.econ@gmail.com>
2019-08-08 17:52:36 +0200

    change structure so I can use arrays, already got and formatted data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,13 +1,18 @@
 "use strict";
 
+//import { format } from "url";
+
 console.log(">> Ready :)");
 
+const savedSeries = [];
+
 const input = document.querySelector(".js-input");
-const series = document.querySelector(".js-series_section");
+//const series = document.querySelector(".js-series_section");
 
 function handleFunction(event) {
   event.preventDefault;
   searchSeries();
+  //listenShow();
 }
 
 function searchSeries() {
@@ -16,16 +21,45 @@ function searchSeries() {
   return fetch(url)
     .then(response => response.json())
     .then(data => {
-      let results = "";
-      for (let item of data) {
-        console.log(item.show.image);
-        results += `<div class="serie_element"><p class="show_title">${
-          item.show.name
-        }</p><img src="${item.show.image.medium}" /></div>`;
-      }
-      series.innerHTML = results;
+      console.log(data);
+      data = formatData(data);
+      //saveData(data);
     });
 }
+//   let results = "";
+//   for (let item of data) {
+//     results += `<div class="serie_element"><p class="show_title">${
+//       item.show.name
+//     }</p><img class="show_image" src="${item.show.image.medium}" /></div>`;
+//   series.innerHTML = results;
+//   const divSerie = document.querySelectorAll(".serie_element");
+//   for (let item of divSerie) {
+//     item.addEventListener("click", addToFavs);
+//   }
+
+function formatData(data) {
+  const results = [];
+  for (let i = 0; i < data.length; i++) {
+    results.push({
+      name: data[i].show.name,
+      image: data[i].show.image.medium
+    });
+  }
+  console.log(results);
+}
+
+// function saveData(data) {
+//   let savedSeries = data;
+//   console.log(savedSeries);
+// }
+
+// function listenShow() {
+//   const showDiv = document.querySelectorAll();
+// }
+
+// function addToFavs(event) {
+//   console.log(event.currentTarget);
+// }
 
 const btn = document.querySelector(".js-btn");
 
