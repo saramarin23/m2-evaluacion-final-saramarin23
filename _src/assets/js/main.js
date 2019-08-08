05b7ac91dcd0e95d12df38b7fc477f228611f7e2
saramarin23 <saramarin.econ@gmail.com>
2019-08-08 16:36:41 +0200

    add search series function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,32 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+const input = document.querySelector(".js-input");
+const series = document.querySelector(".js-series_section");
+
+function handleFunction(event) {
+  event.preventDefault;
+  searchSeries();
+}
+
+function searchSeries() {
+  const inputValue = input.value;
+  const url = `http://api.tvmaze.com/search/shows?q=${inputValue}`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let results = "";
+      for (let item of data) {
+        console.log(item.show.image);
+        results += `<div class="serie_element"><p class="show_title">${
+          item.show.name
+        }</p><img src="${item.show.image.medium}" /></div>`;
+      }
+      series.innerHTML = results;
+    });
+}
+
+const btn = document.querySelector(".js-btn");
+
+btn.addEventListener("click", handleFunction);
