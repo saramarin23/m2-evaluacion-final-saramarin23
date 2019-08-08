8b1f06384129aad2f888ee54915f622fe3aab6a7
saramarin23 <saramarin.econ@gmail.com>
2019-08-08 18:39:33 +0200

    add show data function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,7 +7,6 @@ console.log(">> Ready :)");
 let savedSeries = [];
 
 const input = document.querySelector(".js-input");
-//const series = document.querySelector(".js-series_section");
 
 function handleFunction(event) {
   event.preventDefault;
@@ -23,15 +22,10 @@ function searchSeries() {
     .then(data => {
       data = formatData(data);
       saveData(data);
+      showData();
     });
 }
-//   let results = "";
-//   for (let item of data) {
-//     results += `<div class="serie_element"><p class="show_title">${
-//       item.show.name
-//     }</p><img class="show_image" src="${item.show.image.medium}" /></div>`;
-//   series.innerHTML = results;
-//   const divSerie = document.querySelectorAll(".serie_element");
+
 //   for (let item of divSerie) {
 //     item.addEventListener("click", addToFavs);
 //   }
@@ -50,6 +44,18 @@ function formatData(data) {
 function saveData(data) {
   savedSeries = data;
   console.log(savedSeries);
+}
+
+function showData() {
+  const series = document.querySelector(".js-series_section");
+  let seriesToAdd = "";
+  for (let item = 0; item < savedSeries.length; item++) {
+    seriesToAdd += `<div class="serie_element"><p class="show_title">${
+      savedSeries[item].name
+    }</p><img class="show_image" src="${savedSeries[item].image}" /></div>`;
+  }
+  series.innerHTML = seriesToAdd;
+  console.log(series);
 }
 
 const btn = document.querySelector(".js-btn");
